ecba4ae56a8813cad6f9f565fad4f3d01f2a2098
Abel Coronado <acoronad@ucm.es>
2017-01-24 19:19:38 +0100

    Update 4.c

--- Intro/4.c
+++ Intro/4.c
@@ -7,23 +7,18 @@
 
 
 int main() {
-	errno = 0;
 
 	long maxArgs = sysconf(_SC_ARG_MAX);
 	long maxHijos = sysconf(_SC_CHILD_MAX);
 	long maxFich = sysconf(_SC_OPEN_MAX);
-
-if (errno == 0)
-{
-   printf ("Numero maximo de argumentos: %ld\n", maxArgs);
-   printf ("Numero maximo de hijos: %ld\n", maxHijos);
-   printf ("Numero maximo de ficheros: %ld\n", maxFich);
-}
-else
-{
-    printf("error: %d\n", errno);
-    exit(1);
-}
-
-return 1;
+	
+	if(maxArgs == -1 || maxHijos == -1 || maxFich == -1)
+		perror("Error: %d\n");
+	else {
+		printf("Numero maximo de argumentos: %ld\n", maxArgs);
+		printf("Numero maximo de hijos: %ld\n", maxHijos);
+		printf("Numero maximo de ficheros: %ld\n", maxFich);
+	}
+	
+return 0;
 }
