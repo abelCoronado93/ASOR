78586ee2c81690de203c4a726f16a8a64a004ac2
Abel Coronado <acoronad@ucm.es>
2017-01-24 19:26:27 +0100

    Update sched.c

--- Processes/sched.c
+++ Processes/sched.c
@@ -6,20 +6,20 @@
 int main(){
 
 	printf("%s\n", "----POR DEFECTO----");
-	printf("Planificador: %i\n", sched_getscheduler(0)); //Saco el planificador por defecto (0:OTHER, 1:FIFO, 2:RR)
+	printf("Planificador: %i\n", sched_getscheduler(0));
 	printf("Prioridad: %i\n", getpriority(PRIO_PROCESS, 0));
-	printf("Prioridad máxima: %i\n", sched_get_priority_max(sched_getscheduler(0))); //0
-	printf("Prioridad mínima: %i\n", sched_get_priority_min(sched_getscheduler(0))); //0
+	printf("Prioridad máxima: %i\n", sched_get_priority_max(sched_getscheduler(0))); 
+	printf("Prioridad mínima: %i\n", sched_get_priority_min(sched_getscheduler(0)));
 
 
 	printf("%s\n", "---- CAMBIAMOS A PLANIFICADOR FIFO Y PRIORIDAD 12----");
 	const struct sched_param p;
 	sched_setscheduler(0, 0, &p);
 	setpriority(PRIO_PROCESS, 0, 12); 
-	printf("Planificador: %i\n", sched_getscheduler(0)); //DEVUELVE 0, NO CAMBIA EL PLANIFICADOR
-	printf("Prioridad: %i\n", getpriority(PRIO_PROCESS, 0)); //DEVUELVE 12 (BIEN)
-	printf("Prioridad máxima: %i\n", sched_get_priority_max(sched_getscheduler(0))); //Siempre sale 0
-	printf("Prioridad mínima: %i\n", sched_get_priority_min(sched_getscheduler(0))); //Siempre sale 0
+	printf("Planificador: %i\n", sched_getscheduler(0)); 
+	printf("Prioridad: %i\n", getpriority(PRIO_PROCESS, 0)); 
+	printf("Prioridad máxima: %i\n", sched_get_priority_max(sched_getscheduler(0)));
+	printf("Prioridad mínima: %i\n", sched_get_priority_min(sched_getscheduler(0))); 
 
 	
 	return 0;
